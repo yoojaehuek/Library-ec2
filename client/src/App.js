eae0a4a9cc2fbbf864b47fc40d670c97ded91b5d
hoo5576 <hoo5576@naver.com>
2024-01-09 17:48:50 +0900

    21321321

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main.js';
+import BookDetail from './pages/BookDetail/BookDetail.js';
 import Test from './pages/Test/Test.js';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js'
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Main></Main>}></Route>
+        <Route path='/BookDetail' element={<BookDetail></BookDetail>}></Route>
         <Route path='/test' element={<Test></Test>}></Route>
       </Routes>
       <Footer />
