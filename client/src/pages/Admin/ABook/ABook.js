bf187d91575f9d77cb1b17a72a9ca09a82599b59
hoo5576 <hoo5576@naver.com>
2024-01-16 17:40:16 +0900

    관리자 페이지 50%

--- client/src/pages/Admin/ABook/ABook.js
+++ client/src/pages/Admin/ABook/ABook.js
@@ -1,329 +1,133 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { API_URL } from '../../../config/contansts';
-import { Upload, Pagination } from 'antd'; // Import Pagination from Ant Design
+import axios from 'axios';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Pagination,
+  ButtonGroup,
+} from '@mui/material';
 
-const ASlider = () => {
+const ABook = () => {
   const [axiosResult, setAxiosResult] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [editedId, setEditedId] = useState('');
-  const [editedType, setEditedType] = useState('image');
-  const [editedContent, setEditedContent] = useState('');
-  const [editedTime, setEditedTime] = useState('');
-  const [isNewSlideModalOpen, setIsNewSlideModalOpen] = useState(false);
-  const [newSlideType, setNewSlideType] = useState('image');
-  const [newSlideContent, setNewSlideContent] = useState('');
-  const [newSlideTime, setNewSlideTime] = useState('');
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [genres, setGenres] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 3;
-  const [imageUrl, setImageUrl] = useState("");
+  const itemsPerPage = 5;
 
   useEffect(() => {
-    fetchSliderData();
+    fetchBookData();
   }, []);
 
-  const fetchSliderData = () => {
-    axios.get(`${API_URL}/slider`)
-      .then(res => {
-        console.log(res.data);
+  const fetchBookData = () => {
+    axios
+      .get(`${API_URL}/api/book`)
+      .then((res) => {
         setAxiosResult(res.data);
+        const uniqueGenres = [...new Set(res.data.map((book) => book.book_genre))];
+        setGenres(uniqueGenres);
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
       });
   };
 
-  const openModal = (item) => {
-    setSelectedItem(item);
-    setEditedId(item.id);
-    setEditedType(item.type);
-    setEditedContent(item.content_url);
-    setEditedTime(item.duration);
-    setIsModalOpen(true);
-    setImageUrl("");
-    setIsModalOpen(true);
+  const handleGenreClick = (genre) => {
+    const booksByGenre = axiosResult.filter((book) => book.book_genre === genre);
+    setFilteredBooks(booksByGenre);
+    setCurrentPage(1); // 장르를 바꿀 때 페이지를 1로 초기화
   };
 
-  const closeModal = () => {
-    setSelectedItem(null);
-    setIsModalOpen(false);
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
   };
 
-  const openNewSlideModal = () => {
-    setIsNewSlideModalOpen(true);
-    setImageUrl("");
-  };
+  const renderBooks = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const booksToRender = filteredBooks.length > 0 ? filteredBooks : axiosResult;
 
-  const closeNewSlideModal = () => {
-    setIsNewSlideModalOpen(false);
-    // 모달 닫을 때 입력값 초기화
-    setNewSlideType('image');
-    setNewSlideContent('');
-    setNewSlideTime('');
-  };
-
-  const handleEdit = () => {
-    // 수정 로직 구현
-    if (selectedItem) {
-      const updatedItem = {
-        type: editedType,
-        content_url: imageUrl,
-        duration: editedTime,
-      };
-      console.log(updatedItem);
-      const userConfirmed = window.confirm('수정하시겠습니까?');
-
-      if (userConfirmed) {
-        axios.patch(`${API_URL}/slider/${selectedItem.id}`, updatedItem)
-          .then(() => {
-            alert("수정되었습니다.");
-            fetchSliderData(); // 데이터 갱신
-            closeModal();
-          })
-          .catch(err => {
-            console.error(err);
-            alert("수정에 실패했습니다.");
-          });
-      } else {
-        return;
-      }
-    }
-  };
-
-  const handleAddSlide = () => {
-    // 슬라이드 추가 로직 구현
-    const newSlide = {
-      type: newSlideType,
-      content_url: imageUrl,
-      duration: newSlideTime,
-    };
-
-    axios.post(`${API_URL}/slider`, newSlide)
-      .then(() => {
-        alert("슬라이드가 추가되었습니다.");
-        fetchSliderData(); // 데이터 갱신
-        closeNewSlideModal();
-      })
-      .catch(err => {
-        console.error(err);
-        alert("슬라이드 추가에 실패했습니다.");
-      });
-  };
-
-  const handleDelete = (id) => {
-
-    const userConfirmed = window.confirm('삭제하시겠습니까?');
-    // 삭제 로직 구현
-    if(userConfirmed){
-      axios.delete(`${API_URL}/slider/${id}`)
-      .then(res => {
-        alert("삭제되었습니다.");
-        fetchSliderData(); // 데이터 갱신
-      })
-      .catch(err => {
-        console.error(err);
-        alert("삭제에 실패했습니다.");
-      });
-    }
-  };
-
-  const onChangeImage = (info) => {
-    // 파일이 업로드 중일 때
-    console.log("new", info.file);
-    if (info.file.status === "uploading") {
-      console.log("업로드중");
-      return;
-    }
-    // 파일이 업로드 완료 되었을 때
-    if (info.file.status === "done") {
-      console.log("성공");
-      const response = info.file.response;
-      const imageUrl = response.imageUrl;
-      // 받은 이미지경로를 imageUrl에 넣어줌
-      setImageUrl(imageUrl);
-    }
-  };
-
-
-  // 현재 페이지용 인덱스 계산
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = axiosResult.slice(indexOfFirstItem, indexOfLastItem);
-
-  // 페이지 변경을 처리하는 함수
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
+    return booksToRender.slice(startIndex, endIndex).map((item, index) => (
+      <TableRow key={index}>
+        <TableCell>{item.book_id}</TableCell>
+        <TableCell sx={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "150px" ,whiteSpace: "nowrap",}}>{item.book_name}</TableCell>
+        <TableCell>  
+          <img
+            src={ API_URL + item.book_img_url}
+            alt={item.book_title}
+            style={{ maxWidth: '100px', maxHeight: '100px' }}
+          />
+        </TableCell>
+        <TableCell>{item.book_author}</TableCell>
+        <TableCell>{item.book_publisher}</TableCell>
+        <TableCell>{item.book_genre}</TableCell>
+        <TableCell>{item.book_availability}</TableCell>
+        <TableCell sx={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "250px" ,whiteSpace: "nowrap",}}>{item.book_description}</TableCell>
+        <TableCell>{item.book_ISBN}</TableCell>
+        <TableCell>{item.created_at}</TableCell>
+        <TableCell>
+          <Button
+            variant="contained"
+            color="primary"
+            // onClick={() => handleEdit(item.banner_id)}
+          >
+            수정
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            // onClick={() => handleDelete(item.banner_id)}
+          >
+            삭제
+          </Button>
+        </TableCell>
+      </TableRow>
+    ));
   };
 
   return (
-    <div className='admin-slider-container'>
-      <h1>Slider</h1> 
-      <div className='add-slider-button'>
-        <button onClick={openNewSlideModal}>슬라이더 추가</button>
-      </div>
-      <div className='admin-slider'>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>유형</th>
-              <th>콘텐츠</th>
-              <th>시간(1000=1초)</th>
-              <th>관리</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentItems.map((item, index) => (
-              <tr key={index}>
-                {Object.keys(axiosResult[0]).map((column, colIndex) => (
-                  <td key={colIndex}>
-                    {colIndex === 2 ? (
-                      item.type === "video" ? (
-                        <video autoPlay muted controls>
-                          <source src={API_URL + item[column]}></source>
-                        </video>
-                      ) : (
-                        <img src={API_URL + item[column]} alt={`Slide ${item.id}`} />
-                      )
-                    ) : (
-                      item[column]
-                    )}
-                  </td>
-                ))}
-                <td>
-                  <button onClick={() => openModal(item)}>수정</button>
-                  <button onClick={() => handleDelete(item.id)}>삭제</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      {/* 페이징 */}
-      <div className="pagination">
-        <Pagination
-          current={currentPage}
-          total={axiosResult.length}
-          pageSize={itemsPerPage}
-          onChange={handlePageChange}
-        />
-      </div>
-
-      {/* 모달 */}
-      {isModalOpen && (
-        <div className="modal">
-          <form>
-            <div className="modal-content">
-              <span className="close" onClick={closeModal}>&times;</span>
-              <h2>수정</h2>
-              <label htmlFor="editedId">ID:</label>
-              <input
-                type="text"
-                id="editedId"
-                value={editedId}
-                disabled // 수정 불가능하게
-              />
-              <label htmlFor="editedType">유형:</label>
-              <select
-                id="editedType"
-                value={editedType}
-                onChange={(e) => setEditedType(e.target.value)}
-              >
-                <option value="image">이미지</option>
-                <option value="video">비디오</option>
-              </select>
-              <label className='content' htmlFor="editedContent">콘텐츠:</label>
-              {/* <textarea
-                id="editedContent"
-                value={editedContent}
-                onChange={(e) => setEditedContent(e.target.value)}
-              ></textarea> */}
-              <Upload
-                name="image"
-                action={`${API_URL}/image`}
-                listType="picture"
-                showUploadList={false}
-                onChange={onChangeImage}
-              >
-                {imageUrl ? (
-                  <p>업로드된 이미지: {imageUrl}</p>
-                  ) : (
-                  <div id="upload-img-placeholder">
-                    <i className="fa-regular fa-file-image"></i>
-                    <br />
-                    <span>클릭하거나 드래그하여 업로드하세요.</span>
-                  </div>
-                )}
-              </Upload>
-              <label htmlFor="editedTime">시간(1000=1초):</label>
-              <input
-                type="text"
-                id="editedTime"
-                value={editedTime}
-                onChange={(e) => setEditedTime(e.target.value)}
-              />
-              <button type="button" onClick={handleEdit}>저장</button>
-              <button type="button" onClick={closeModal}>닫기</button>
-            </div>
-          </form>
-        </div>
-      )}
-
-      {isNewSlideModalOpen && (
-        <div className="modal">
-          <form>
-            <div className="modal-content">
-              <span className="close" onClick={closeNewSlideModal}>&times;</span>
-              <h2>슬라이드 추가</h2>
-              <label htmlFor="newSlideType">유형:</label>
-              <select
-                id="newSlideType"
-                value={newSlideType}
-                onChange={(e) => setNewSlideType(e.target.value)}
-              >
-                <option value="image">이미지</option>
-                <option value="video">비디오</option>
-              </select>
-              <label className='content' htmlFor="newSlideContent">콘텐츠:</label>
-              {/* <textarea
-                id="newSlideContent"
-                value={newSlideContent}
-                onChange={(e) => setNewSlideContent(e.target.value)}
-              ></textarea> */}
-              <Upload
-                name="image"
-                action={`${API_URL}/image`}
-                listType="picture"
-                showUploadList={false}
-                onChange={onChangeImage}
-              >
-                {imageUrl ? (
-                  <p>업로드된 이미지: {imageUrl}</p>
-                  ) : (
-                  <div id="upload-img-placeholder">
-                    <i className="fa-regular fa-file-image"></i>
-                    <br />
-                    <span>클릭하거나 드래그하여 업로드하세요.</span>
-                  </div>
-                )}
-              </Upload>
-              <label htmlFor="newSlideTime">시간(1000=1초):</label>
-              <input
-                type="text"
-                id="newSlideTime"
-                value={newSlideTime}
-                onChange={(e) => setNewSlideTime(e.target.value)}
-              />
-              <button type="button" onClick={handleAddSlide}>추가</button>
-              <button type="button" onClick={closeNewSlideModal}>취소</button>
-            </div>
-          </form>
-        </div>
-      )}
+    <div style={{ textAlign: 'center',maxWidth: "75vw", margin: "auto", overflowX: 'auto'  }}>
+      <ButtonGroup>
+        {genres.map((genre, index) => (
+          <Button key={index} onClick={() => handleGenreClick(genre)}>
+            {genre}
+          </Button>
+        ))}
+      </ButtonGroup>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>책 ID</TableCell>
+              <TableCell>책 이름</TableCell>
+              <TableCell>책 표지</TableCell>
+              <TableCell>저자</TableCell>
+              <TableCell>출판사</TableCell>
+              <TableCell>장르</TableCell>
+              <TableCell>대출 여부</TableCell>
+              <TableCell>책 간단 소개</TableCell>
+              <TableCell>고유 번호</TableCell>
+              <TableCell>작성 시간</TableCell>
+              <TableCell>관리</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody >{renderBooks()}</TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        count={Math.ceil(filteredBooks.length > 0 ? filteredBooks.length / itemsPerPage : axiosResult.length / itemsPerPage)}
+        page={currentPage}
+        onChange={handlePageChange}
+        color="primary"
+      />
     </div>
   );
 };
 
-export default ASlider;
+export default ABook;
