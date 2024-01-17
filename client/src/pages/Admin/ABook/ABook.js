5cdcc4b4af2e1a3b252ac11ddff9189cdf125b76
hoo5576 <hoo5576@naver.com>
2024-01-17 17:30:20 +0900

    100%

--- client/src/pages/Admin/ABook/ABook.js
+++ client/src/pages/Admin/ABook/ABook.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL } from '../../../config/contansts';
 import axios from 'axios';
+import AddIcon from '@mui/icons-material/Add';
 import {
   Table,
   TableBody,
@@ -9,7 +10,21 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
   Button,
+  TextareaAutosize,
+  Typography,
+  Select,
+  MenuItem,
+  Fab,
+  Input,
+  FormControl,
+  InputLabel,
+
   Pagination,
   ButtonGroup,
 } from '@mui/material';
@@ -19,6 +34,18 @@ const ABook = () => {
   const [filteredBooks, setFilteredBooks] = useState([]);
   const [genres, setGenres] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [editBookId, setEditBookId] = useState(null);
+  const [bookName, setBookName] = useState('');
+  const [bookImgurl, setBookImgurl] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  const [bookPublisher, setBookPublisher] = useState('');
+  const [bookGenre, setBookGenre] = useState('');
+  const [bookAvailability, setBookAvailability] = useState('');
+  const [bookDescription, setBookDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
+  const [bookISBN, setBookISBN] = useState('');
   const itemsPerPage = 5;
 
   useEffect(() => {
@@ -48,6 +75,153 @@ const ABook = () => {
     setCurrentPage(value);
   };
 
+  const handleEditClick = (book) => {
+    setEditBookId(book.book_id);
+    setSelectedBook(book);
+    setBookName(book.book_name);
+    setBookImgurl(book.book_img_url);
+    setBookAuthor(book.book_author);
+    setBookPublisher(book.book_publisher);
+    setBookGenre(book.book_genre);
+    setBookDescription(book.book_description);
+    setBookISBN(book.book_ISBN);
+    setBookAvailability(book.book_availability);
+    setOpenEditDialog(true);
+  };
+
+  const [openAddDialog, setOpenAddDialog] = useState(false);
+  const handleOpenAddDialog = () => setOpenAddDialog(true);
+  const handleCloseAddDialog = () => {
+    setOpenAddDialog(false);
+    setBookName('');
+    setBookAuthor('');
+    setBookPublisher('');
+    setBookGenre('');
+    setBookDescription('');
+    setBookISBN('');
+    setBookAvailability('');
+    setImageUrl(''); // 이미지 URL 초기화
+  }
+  
+
+  const handleSaveEdit = () => {
+    if (editBookId) {
+      const updatedItem = {
+        book_name: bookName,
+        book_img_url: bookImgurl,
+        book_author: bookAuthor,
+        book_publisher: bookPublisher,
+        book_genre: bookGenre,
+        book_availability: bookAvailability,
+        book_description: bookDescription,
+        book_ISBN: bookISBN,
+      };
+  
+  
+      console.log('update:', updatedItem);
+      const userConfirmed = window.confirm('수정하시겠습니까?');
+  
+      if (userConfirmed) {
+        console.log(editBookId)
+        axios
+          .patch(`${API_URL}/api/book/${editBookId}`, updatedItem)
+          .then(() => {
+            alert('수정되었습니다.');
+            fetchBookData(); // 데이터 갱신
+            handleCloseEditDialog();
+          })
+          .catch((err) => {
+            console.error(err);
+            alert('수정에 실패했습니다.');
+          });
+      } else {
+        return;
+      }
+    }
+  };
+
+  const handleDelete = (bookId) => {
+    const userConfirmed = window.confirm('삭제하시겠습니까?');
+  
+    if (userConfirmed) {
+      axios
+        .delete(`${API_URL}/api/book/${bookId}`)
+        .then(() => {
+          alert('삭제되었습니다.');
+          fetchBookData(); // 데이터 갱신
+          setFilteredBooks(filteredBooks.filter(book => book.book_id !== bookId)); // filteredBooks 업데이트
+        })
+        .catch((err) => {
+          console.error(err);
+          alert('삭제에 실패했습니다.');
+        });
+    }
+  };
+
+  const onChangeImage = async (info) => {
+    // 파일이 업로드 중일 때
+    // console.log("upload/index.js/onChangeImage() info.file: ", info.file);
+
+    console.log('info', info.target.files[0]);
+    const file = info.target.files[0];
+
+    try {
+      const formData = new FormData();
+      formData.append('bookimg', file);
+  
+      const res = await axios.post(`${API_URL}/api/bookimg`, formData);
+      console.log("res: ", res);
+      setImageUrl(res.data.imageUrl)
+    } catch (error) {
+      console.error("에러발생: ", error);
+    }
+  };
+
+
+  const handleAdd = () => {
+    const createItem = {
+      book_name: bookName,
+      book_img_url: imageUrl,
+      book_author: bookAuthor,
+      book_publisher: bookPublisher,
+      book_genre: bookGenre,
+      book_availability: bookAvailability,
+      book_description: bookDescription,
+      book_ISBN: bookISBN,
+    };
+    
+
+    const userConfirmed = window.confirm('추가하시겠습니까?');
+    if (userConfirmed) {
+      axios
+        .post(`${API_URL}/api/book`, createItem)
+        .then(() => {
+          alert('추가되었습니다.');
+          fetchBookData();
+          handleCloseAddDialog();
+        })
+        .catch((err) => {
+          console.error(err);
+          alert('추가에 실패했습니다.');
+        });
+        
+    }
+  };
+
+
+  const handleCloseEditDialog = () => {
+    setOpenEditDialog(false);
+    setOpenAddDialog(false);
+    setBookName('');
+    setBookAuthor('');
+    setBookPublisher('');
+    setBookGenre('');
+    setBookDescription('');
+    setBookISBN('');
+    setBookAvailability('');
+    setImageUrl(''); // 이미지 URL 초기화
+  };
+
   const renderBooks = () => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
@@ -61,13 +235,15 @@ const ABook = () => {
           <img
             src={ API_URL + item.book_img_url}
             alt={item.book_title}
-            style={{ maxWidth: '100px', maxHeight: '100px' }}
+            style={{ width: '80px', height: '120px' }}
           />
         </TableCell>
         <TableCell>{item.book_author}</TableCell>
-        <TableCell>{item.book_publisher}</TableCell>
+        <TableCell sx={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "250px" ,whiteSpace: "nowrap",}} >{item.book_publisher}</TableCell>
         <TableCell>{item.book_genre}</TableCell>
-        <TableCell>{item.book_availability}</TableCell>
+        <TableCell>
+          {item.book_availability === 1 ? "가능" : "불가능"}
+        </TableCell>
         <TableCell sx={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "250px" ,whiteSpace: "nowrap",}}>{item.book_description}</TableCell>
         <TableCell>{item.book_ISBN}</TableCell>
         <TableCell>{item.created_at}</TableCell>
@@ -75,14 +251,14 @@ const ABook = () => {
           <Button
             variant="contained"
             color="primary"
-            // onClick={() => handleEdit(item.banner_id)}
+            onClick={() => handleEditClick(item)}
           >
             수정
           </Button>
           <Button
             variant="contained"
             color="error"
-            // onClick={() => handleDelete(item.banner_id)}
+            onClick={() => handleDelete(item.book_id)}
           >
             삭제
           </Button>
@@ -110,14 +286,22 @@ const ABook = () => {
               <TableCell>저자</TableCell>
               <TableCell>출판사</TableCell>
               <TableCell>장르</TableCell>
-              <TableCell>대출 여부</TableCell>
+              <TableCell>대출여부</TableCell>
               <TableCell>줄거리</TableCell>
-              <TableCell>고유 번호</TableCell>
-              <TableCell>작성 시간</TableCell>
+              <TableCell>고유번호</TableCell>
+              <TableCell>작성시간</TableCell>
               <TableCell>관리</TableCell>
             </TableRow>
           </TableHead>
           <TableBody >{renderBooks()}</TableBody>
+          <Fab
+            color="primary"
+            aria-label="add"
+            sx={{ position: 'fixed', bottom: 16, right: 16 }}
+            onClick={handleOpenAddDialog}
+          >
+            <AddIcon/>
+          </Fab>
         </Table>
       </TableContainer>
       <Pagination
@@ -127,6 +311,210 @@ const ABook = () => {
         color="primary"
         sx={{ marginTop: '16px', display: 'flex', justifyContent: 'center' }}
       />
+
+      <Dialog open={openAddDialog} onClose={handleCloseAddDialog}>
+        <DialogTitle>책 추가</DialogTitle>
+        <DialogContent>
+        <TextField
+            label="책 이름"
+            value={bookName}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookName(e.target.value)}
+          />
+          <InputLabel>이미지 업로드</InputLabel>
+          {/* <form method="post" name='accountFrm' action={`${API_URL}/api/bannerimg`} encType='multipart/form-data'> */}
+            <Input
+              type="file"
+              // action={`${API_URL}/api/bannerimg`}
+              accept="image/*"
+              name='bookimg'
+              onChange={onChangeImage}
+            />
+            {/* <Input type='submit' value='확인' /> */}
+          {/* </form> */}
+          {imageUrl && (
+            <img
+              src={ API_URL + imageUrl}
+              alt="Banner"
+              style={{ width: '100%', height:"500px", marginTop: '10px' }}
+            />
+          )}
+          <TextField
+            label="저자"
+            value={bookAuthor}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookAuthor(e.target.value)}
+          />
+          <TextField
+            label="출판사"
+            value={bookPublisher}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookPublisher(e.target.value)}
+          />
+          <FormControl fullWidth margin="normal">
+            <InputLabel>장르</InputLabel>
+            <Select
+              label='장르'
+              value={bookGenre}
+              onChange={(e) => setBookGenre(e.target.value)}
+            >
+              {genres.map((genre, index) => (
+                <MenuItem key={index} value={genre}>
+                  {genre}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <InputLabel>대출여부</InputLabel>
+            <Select
+              label="대출여부"
+              value={bookAvailability}
+              onChange={(e) => setBookAvailability(e.target.value)}
+            >
+              <MenuItem value="1">대출 가능</MenuItem>
+              <MenuItem value="0">대출 불가능</MenuItem>
+            </Select>
+          </FormControl>
+          <Typography variant="subtitle1" gutterBottom>
+            줄거리
+          </Typography>
+          <TextareaAutosize
+            value={bookDescription}
+            onChange={(e) => setBookDescription(e.target.value)}
+            label="줄거리"
+            minRows={3} // 최소 높이 지정
+            maxRows={10} // 최대 높이 지정
+            style={{
+              width: '97%',
+              resize: 'vertical', // 수직 크기 조절만 가능하도록 설정
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc',
+            }}
+          />
+          <TextField
+            label="고유번호"
+            value={bookISBN}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookISBN(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseAddDialog} color="primary">
+            취소
+          </Button>
+          <Button onClick={handleAdd} color="primary">
+            추가
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openEditDialog} onClose={handleCloseEditDialog}>
+        <DialogTitle>책 수정</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="책 이름"
+            value={bookName}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookName(e.target.value)}
+          />
+          <InputLabel>이미지 업로드</InputLabel>
+          {/* <form method="post" name='accountFrm' action={`${API_URL}/api/bannerimg`} encType='multipart/form-data'> */}
+            <Input
+              type="file"
+              // action={`${API_URL}/api/bannerimg`}
+              accept="image/*"
+              name='bookimg'
+              onChange={onChangeImage}
+            />
+            {/* <Input type='submit' value='확인' /> */}
+          {/* </form> */}
+          {imageUrl && (
+            <img
+              src={ API_URL + imageUrl}
+              alt="Banner"
+              style={{ width: '100%', height:"500px", marginTop: '10px' }}
+            />
+          )}
+          <TextField
+            label="저자"
+            value={bookAuthor}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookAuthor(e.target.value)}
+          />
+          <TextField
+            label="출판사"
+            value={bookPublisher}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookPublisher(e.target.value)}
+          />
+          <FormControl fullWidth margin="normal">
+            <InputLabel>장르</InputLabel>
+            <Select
+              label='장르'
+              value={bookGenre}
+              onChange={(e) => setBookGenre(e.target.value)}
+            >
+              {genres.map((genre, index) => (
+                <MenuItem key={index} value={genre}>
+                  {genre}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <InputLabel>대출여부</InputLabel>
+            <Select
+              label="대출여부"
+              value={bookAvailability}
+              onChange={(e) => setBookAvailability(e.target.value)}
+            >
+              <MenuItem value="1">대출 가능</MenuItem>
+              <MenuItem value="0">대출 불가능</MenuItem>
+            </Select>
+          </FormControl>
+          <Typography variant="subtitle1" gutterBottom>
+            줄거리
+          </Typography>
+          <TextareaAutosize
+            value={bookDescription}
+            onChange={(e) => setBookDescription(e.target.value)}
+            label="줄거리"
+            minRows={3} // 최소 높이 지정
+            maxRows={10} // 최대 높이 지정
+            style={{
+              width: '97%',
+              resize: 'vertical', // 수직 크기 조절만 가능하도록 설정
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc',
+            }}
+          />
+          <TextField
+            label="고유번호"
+            value={bookISBN}
+            fullWidth
+            margin="normal"
+            onChange={(e) => setBookISBN(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditDialog} color="primary">
+            취소
+          </Button>
+          <Button onClick={handleSaveEdit} color="primary">
+            저장
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
--- client/src/pages/Main/MainBook.js
+++ client/src/pages/Main/MainBook.js
@@ -2,32 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import axios  from 'axios';
+import axios from 'axios';
 import { API_URL } from '../../config/contansts';
 import './Main.scss';
 
-const MainBook = ({Genre}) => {
+const MainBook = ({ Genre }) => {
   const [axiosResult, setAxiosResult] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(`${API_URL}/api/book?book_genre=${Genre}&limit=5`)
-    .then(res => {
-      setAxiosResult(res.data);
-      console.log("응답 데이터: ", res.data);
-    }).catch((err) =>{
-            console.error(err);
-        });
-  },[]);
+      .then(res => {
+        setAxiosResult(res.data);
+        console.log("응답 데이터: ", res.data);
+      }).catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-
-return (
-    <div className='main-mid-container-lhs '>
+  return (
+    <div className='main-mid-container-lhs'>
       <div className='main-mid-top-lhs'>
         <h1>{Genre}</h1>
       </div>
       <div className='main-mid-img-lhs'>
         {axiosResult.map((item, index) => (
-          <NavLink to={`/BookDetail/${item.book_id}`}key={index} className='grid-item-lhs'>
+          <NavLink to={`/BookDetail/${item.book_id}`} key={index} className='grid-item-lhs'>
             <img src={API_URL + item.book_img_url} alt={`grid${index + 1}`} />
             <div className='image-text-lhs'>{item.book_name}</div>
           </NavLink>
