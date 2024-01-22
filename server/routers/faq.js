3acc2c65218c86c54ca3131ec866f5c7b09c136d
kimjunnyoung <kimjunnyoung@gmail.com>
2024-01-22 14:36:26 +0900

    로그인 미들웨어

--- server/routers/faq.js
+++ server/routers/faq.js
@@ -7,7 +7,7 @@ router.post('/', authMiddleware, FaqController.createFaq);
 router.get('/', FaqController.getAllFaq);
 router.get('/category/:category', FaqController.getCategoryFaq);
 router.get('/:faq_id', FaqController.getOneFaq);
-router.patch('/:faq_id',  FaqController.updateFaq);
-router.delete('/:faq_id',  FaqController.deleteFaq);
+router.patch('/:faq_id', FaqController.updateFaq);
+router.delete('/:faq_id', FaqController.deleteFaq);
 
 module.exports = router;
