450610f09e9c3c8fca351cda4704c8889a8a376c
chajeongheon <jeongheon1127@gmail.com>
2024-01-22 17:50:35 +0900

    123

--- client/src/pages/Mypage/Mypage.js
+++ client/src/pages/Mypage/Mypage.js
@@ -5,43 +5,109 @@ import axios from 'axios';
 import { API_URL } from '../../config/contansts';
 import LoansTable from '../../components/Mypage/LoansTable/LoansTable';
 import { LiaUserEditSolid } from "react-icons/lia";
+import { errHandler } from "../../utils/globalFunction";
+import { useRecoilState } from "recoil";
+import { loginState } from "../../recoil/atoms/State";
 
 const Mypage = () => {
+  const [islogin, setIslogin] = useRecoilState(loginState);
   const [user, setUser] = useState({});
   const [userId, setUserId] = useState([]);
   const [event, setEvent] = useState([]);
   const [faq, setFaq] = useState([]);
 
-  useEffect(()=>{
-    axios.get(`${API_URL}/api/user/one`)
-    .then(res => {
+
+  const getUser = async () => {
+    const res = await axios.get(`${API_URL}/api/user/one`);
+
+    if (res.status == 200) {
       setUser(res.data);
       setUserId(res.data.user_id);
-      console.log("받은 유저정보: ", res.data);
-    }).catch((err) =>{
-      console.error(err);
-    });
-  },[]);
+    }else if (res.status == 302 ){
+      console.log("에러res: ", res);
+      // setIslogin(false);
+      // errHandler(error);
+    }else {
+      alert("에러");
+    }
+  }
+  const getUserEvent = async () => {
+    const res = await axios.get(`${API_URL}/api/event_applicants/user`);
 
-  useEffect(()=>{
-    axios.get(`${API_URL}/api/event_applicants/user`)
-    .then(res => {
+    if (res.status == 200) {
       setEvent(res.data);
       console.log("이벤트 : ", res.data);
-    }).catch((err) => {
-      console.error(err);
-    });
-  },[]);
+    }else if (res.status == 302 ){
+      console.log("이벤트 에러res: ", res);
+      // setIslogin(false);
+      // errHandler(error);
+    }else {
+      alert("에러");
+    }
+  }
+  const getUserFaq = async () => {
+    const res = await axios.get(`${API_URL}/api/faq/user`);
 
-  useEffect(()=>{
-    axios.get(`${API_URL}/api/faq/user`)
-    .then(res => {
+    if (res.status == 200) {
       setFaq(res.data);
       console.log("FAQ : ", res.data);
-    }).catch((err) => {
-      console.error(err);
-    });
+    }else if (res.status == 302 ){
+      console.log("이벤트 에러res: ", res);
+      // setIslogin(false);
+      // errHandler(error);
+    }else {
+      alert("에러");
+    }
+  }
+
+  useEffect(()=>{
+    getUser();
+    getUserEvent();
+    getUserFaq();
+    // axios.get(`${API_URL}/api/user/one`)
+    // .then(res => {
+    //   setUser(res.data);
+    //   setUserId(res.data.user_id);
+    //   console.log("받은 유저정보: ", res.data);
+    // }).catch((err) =>{
+    //   console.error(err);
+    // });
+  //   axios.get(`${API_URL}/api/event_applicants/user`)
+  //   .then(res => {
+  //     setEvent(res.data);
+  //     console.log("이벤트 : ", res.data);
+  //   }).catch((err) => {
+  //     console.error(err);
+  //   });
+
+  //   axios.get(`${API_URL}/api/faq/user`)
+  //   .then(res => {
+  //     setFaq(res.data);
+  //     console.log("FAQ : ", res.data);
+  //   }).catch((err) => {
+  //     console.error(err);
+  //   });
   },[]);
+
+  // useEffect(()=>{
+  //   axios.get(`${API_URL}/api/event_applicants/user`)
+  //   .then(res => {
+  //     setEvent(res.data);
+  //     console.log("이벤트 : ", res.data);
+  //   }).catch((err) => {
+  //     console.error(err);
+  //   });
+  // },[setEvent]);
+
+  // useEffect(()=>{
+  //   axios.get(`${API_URL}/api/faq/user`)
+  //   .then(res => {
+  //     setFaq(res.data);
+  //     console.log("FAQ : ", res.data);
+  //   }).catch((err) => {
+  //     console.error(err);
+  //   });
+  // },[setFaq]);
   
   return (
     <div className='mypage-container-kjh'>
@@ -56,7 +122,22 @@ const Mypage = () => {
 
         <LoansTable is_returned={false}/>
         <LoansTable is_returned={true}/>
-
+        <div className='myEvent-kjh'>
+          <table>
+            <tr>
+              <td>내가 신청한 이벤트</td>
+            </tr>
+            {event.map((item, index) => (
+              <tr>
+                <td><img src={API_URL+item.Event.event_img_url}></img></td>
+                <td>{item.Event.event_title}</td>
+                <td>{item.Event.event_status}</td>
+                <td>{item.Event.event_start_date}</td>
+                <td>{item.Event.event_end_date}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   )
