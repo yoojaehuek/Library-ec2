ff4dfea84202ee5a9e159efb18126ee30d370029
MagnoliaDenudata <sjhj000121@gmail.com>
2024-01-22 10:59:00 +0900

    eventcancel, usereventview

--- server/routers/event_applicants.js
+++ server/routers/event_applicants.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Event_applicantsController = require('../database/controllers/event_applicantsController');
+const authMiddleware = require('../utils/authMiddleware');
 
 router.post('/', Event_applicantsController.createEvent_applicants);
 router.get('/', Event_applicantsController.getAllEvent_applicants);
+router.get('/user', authMiddleware, Event_applicantsController.getAllByUser);
 router.get('/:event_id', Event_applicantsController.getOneEvent_applicants);
 router.patch('/:event_id', Event_applicantsController.updateEvent_applicants);
 
