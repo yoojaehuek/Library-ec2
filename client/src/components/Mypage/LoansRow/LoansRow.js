bb6ff4dc7c70a9c2fa0e01d07090509d3f8aa633
chajeongheon <jeongheon1127@gmail.com>
2024-01-22 16:05:35 +0900

    123

--- client/src/components/Mypage/LoansRow/LoansRow.js
+++ client/src/components/Mypage/LoansRow/LoansRow.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { API_URL } from '../../../config/contansts';
+import './LoansRow.scss';
+import { PiPencilLineDuotone } from "react-icons/pi";
+import { IoSearch } from "react-icons/io5";
 
 const LoansRow = ({book, index}) => {
   const [reviewCheckData, setReviewCheckData] = useState();
@@ -17,18 +20,17 @@ const LoansRow = ({book, index}) => {
   }, []);
 
   return (
-    <tr key={index}>
+    <tr key={index} className='loansrow-kjh'>
       <td className='tWriter-kjh'>{book.Book.book_author}</td>
       <td className='tTitle-kjh'>{book.Book.book_name}</td>
       <td className='tSdate-kjh'>{book.loan_date}</td>
       <td className='tEdate-kjh'>{book.due_date}</td>
       <td className='reviewbtn-kjh'>
         {reviewCheckData ? 
-          <NavLink to={`/BookDetail/${book.Book.book_id}`} className="reviewBtn-kjh">작성한 리뷰 보러 가기</NavLink>
+          <NavLink to={`/BookDetail/${book.Book.book_id}`} className="reviewBtn-kjh"><IoSearch/>리뷰 보러 가기</NavLink>
           : 
-          <NavLink to={`/review/${book.Book.book_id}`} className="reviewBtn-kjh">리뷰 쓰러 가기</NavLink>
+          <NavLink to={`/review/${book.Book.book_id}`} className="reviewBtn-kjh"><PiPencilLineDuotone/>리뷰 쓰러 가기</NavLink>
         }
-        
       </td>
     </tr>
   )
--- client/src/pages/Mypage/Mypage.js
+++ client/src/pages/Mypage/Mypage.js
@@ -4,38 +4,38 @@ import './Mypage.scss';
 import axios from 'axios';
 import { API_URL } from '../../config/contansts';
 import LoansTable from '../../components/Mypage/LoansTable/LoansTable';
+import { LiaUserEditSolid } from "react-icons/lia";
 
 const Mypage = () => {
   const [user, setUser] = useState({});
   const [userId, setUserId] = useState({});
-
-  // useEffect(()=>{
-  //   axios.get(`${API_URL}/api/user/one`)
-  //   .then(res => {
-  //     setUser(res.data);
-  //     setUserId(res.data.user_id);
-  //     console.log("받은 유저정보: ", res.data);
-  //   }).catch((err) =>{
-  //     console.error(err);
-  //   });
-  // },[]);
-
+  const [event, setEvent] = useState({});
+  useEffect(()=>{
+    axios.get(`${API_URL}/api/user/one`)
+    .then(res => {
+      setUser(res.data);
+      setUserId(res.data.user_id);
+      console.log("받은 유저정보: ", res.data);
+    }).catch((err) =>{
+      console.error(err);
+    });
+  },[]);
   useEffect(()=>{
     axios.get(`${API_URL}/api/event_applicants/user`)
     .then(res => {
-      setUser(res.data);
+      setEvent(res.data);
       setUserId(res.data.user_id);
       console.log("getAllByUserTesting", res.data);
     }).catch((err) => {
       console.error(err);
     });
   },[]);
-    
+  console.log("아앙아앙: ", user);
   return (
     <div className='mypage-container-kjh'>
       <div className='pageTop-kjh'>
         <h1>{user.name}님 환영합니다.</h1>
-        <NavLink to='/check' className="myEdit-kjh">내 정보 수정 {'>'}</NavLink>
+        <NavLink to='/check' className="myEdit-kjh"><LiaUserEditSolid/>내 정보 수정/탈퇴 {'>'}</NavLink>
       </div>
 
   
